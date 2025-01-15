8f4ec3e8411a3c0cf43090d8a9c7232aed49f23a
sunjuzhong <129019668+sunjuzhong@users.noreply.github.com>
2025-01-15 17:09:52 +0800

    feat: update export

--- projects/wn-zorro/src/lib/components/index.ts
+++ projects/wn-zorro/src/lib/components/index.ts
@@ -1,3 +1,5 @@
 // export * from './form/';
 export * from './list/';
 export * from './template/';
+export * from './view/';
+export * from './table/';
---
+++ projects/wn-zorro/src/lib/components/table/index.ts
@@ -0,0 +1,2 @@
+export * from './type';
+export * from './frontend-table/frontend-table.component';
