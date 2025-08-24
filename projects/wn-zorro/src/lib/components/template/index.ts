1fbc267ba2d30889db536ec1c49969993925fad0
sunjuzhong <129019668+sunjuzhong@users.noreply.github.com>
2025-08-24 22:08:30 +0800

    feat: update the temlat

--- projects/wn-zorro/src/lib/components/template/index.ts
+++ projects/wn-zorro/src/lib/components/template/index.ts
@@ -1,3 +1,4 @@
 export * from './template.module';
 export * from './template.service';
 export * from './header-template/header-template.component';
+export * from './templates-index/templates-index.component';
